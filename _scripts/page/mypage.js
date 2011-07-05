de6f5cc458844d1e7ec91ae84bd2d264baf6b306
Vince Liu <applecompass@gmail.com>
2011-07-05 23:28:36 +0800

    submit

---
+++ _scripts/page/mypage.js
@@ -0,0 +1,192 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// File:            reg.js
+// Defines:
+// Dependencies:
+// Description:     this is the document ready functions for reg page
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+(function($) {
+
+    var Plan = function() {
+
+        var $elBox = $("#fnb-planlist");
+        var $elForm = $("#form-function");
+        var $elHour = $("#fnb-hour");
+
+        var edit = function() {
+            $elBox.find(" > a").live("click", function() {
+
+                var val = $(this).find("span.ph-val").text();
+                switch (val) {
+                    case "1":
+                    default:
+                        $elHour.addClass("ph-one");
+                        $elHour.find("span.ph-val").text(val);
+                        break;
+                    case "10":
+                        $elHour.addClass("ph-ten");
+                        $elHour.find("span.ph-val").text(val);
+                        break;
+                    case "100":
+                        $elHour.addClass("ph-hdrd");
+                        $elHour.find("span.ph-val").text(val);
+                        break;
+                }
+
+                $elBox.hide();
+                $elForm.show("fast").animate(
+                    {
+                        left: 30
+                    },
+                    {
+                        duration: 200
+                    }
+                );
+            });
+        };
+
+        this.reset = function() {
+            $elForm.animate(
+                {
+                    left: "-660px"
+                },
+                {
+                    duration: 200,
+                    complete: function() {
+                        $elForm.css("left", "100%").hide();
+                        $elBox.show()
+                    }
+                }
+            );
+        };
+
+        this.init = function() {
+            edit();
+        };
+
+    };
+
+    var Burn = function() {
+
+        var $elSubmit = $("#fnb-submit");
+        var cssClass = "btn-disable";
+
+        var validNumeric = function() {
+
+            var el = $("#fnb-input")[0],
+                $elErr = $("#fnb-err");
+
+            var handle = function() {
+                var txt = el.value;
+                var maxMin = parseInt($("#fnb-hour span.ph-val").text()) * 60;
+
+                if (txt !== "") {
+                    if (!isNaN(txt)) {
+                        if (txt <= maxMin) {
+                            $elSubmit.removeClass(cssClass);
+                            $elErr.html("");
+                        }
+                        else {
+                            $elSubmit.addClass(cssClass);
+                            $elErr.html(ZM.Config.Msg.Valid.outside);
+                        }
+                    }
+                    else {
+                        $elSubmit.addClass(cssClass);
+                        $elErr.html(ZM.Config.Msg.Valid.numeric);
+                    }
+                }
+                else {
+                    $elSubmit.addClass(cssClass);
+                    $elErr.html("");
+                }
+            };
+
+            // input text change event
+            if ($.browser.msie) {
+                el.onpropertychange = handle;
+            }
+            else {
+                el.addEventListener("input", handle, false);
+            }
+            el.addEventListener("blur", handle, false);
+
+        };
+
+        var clickSubmit = function() {
+
+            $("#fnb-submit").click(function() {
+                if (!$(this).hasClass(cssClass)) {
+                    $("#fnb-input-submit").click();
+                    var objPlan = new Plan();
+                    objPlan.reset();
+                }
+                return false;
+            });
+
+        };
+
+        this.init = function() {
+            validNumeric();
+            clickSubmit();
+        };
+    };
+
+    var Progress = function() {
+
+        var event = function() {
+            $("#activitystatusbox ul li").bind({
+                "mouseover": function() {
+                    $(this).find(".asb-giveup").show();
+                },
+                "mouseout": function() {
+                    $(this).find(".asb-giveup").hide();
+                }
+            });
+            $("#activitystatusbox ul li .asb-giveup").bind({
+                "mouseover": function() {
+                    $(this).find(".asb-gu-prompt").show();
+                },
+                "mouseout": function() {
+                    $(this).find(".asb-gu-prompt").hide();
+                }
+            });
+        };
+
+        this.init = function() {
+            event();
+        };
+    };
+
+    ZM.UI.Mypage = {
+        init: function() {
+            var objBurn = new Burn();
+            objBurn.init();
+
+            var objPlan = new Plan();
+            objPlan.init();
+
+            var objProgress = new Progress();
+            objProgress.init();
+        }
+    };
+
+})(jQuery);
+
+// Document Ready.
+jQuery(function($) {
+
+    ZM.UI.Mypage.init();
+
+});
+
+// Window Onload.
+ZM.windowOnload = window.onload;
+window.onload = function() {
+    if (ZM.windowOnload) {
+        ZM.windowOnload();
+    }
+    // Your code here
+};
---
+++ _scripts/ui/form.js
@@ -0,0 +1,144 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// File:            form.js
+// Defines:
+// Dependencies:
+// Description:     JS UI functions for forms
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+(function($) {
+
+    var Form = function() {
+
+        var hint = function() {
+            //ZM.Dom.textHint('#sb-mail', '#sb-mail-hint');
+        };
+
+        var validEmail = function() {
+
+            var $el = $elMail;
+
+            $el.blur(function() {
+                var txt = this.value;
+
+                if (txt !== "") {
+                    if (!ZM.Dom.mailValid(txt)) {
+                        $(this).next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Format.mail);
+                    }
+                    else {
+                        $(this).next().hide();
+                    }
+                }
+                else {
+                    $(this).next().hide();
+                }
+            });
+
+        };
+
+        var validUserName = function() {
+
+            var $el = $elName;
+
+            $el.blur(function() {
+                var txt = this.value;
+
+                if (txt.length >= 12) {
+                    $(this).next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Format.username.format(ZM.Config.Value.maxUsername));
+                }
+                else {
+                    $(this).next().hide();
+                }
+            });
+
+        };
+
+        var validPassword = function() {
+
+            var $el = $elPass;
+
+            $el.blur(function() {
+                var txt = this.value,
+                    l = txt.length;
+
+                if (l > 12 || l < 6) {
+                    $(this).next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Format.password);
+                }
+                else {
+                    $(this).next().hide();
+                }
+            });
+
+        };
+
+        var validNull = function() {
+
+            if ($elMail[0].value === "") {
+                $elMail.next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Valid.mail);
+            }
+            if ($elName[0].value === "") {
+                $elName.next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Valid.username);
+            }
+            if ($elPass[0].value === "") {
+                $elPass.next().show().find(".fb-tip-msg").empty().append(ZM.Config.Msg.Valid.password);
+            }
+
+
+        };
+
+        var check = function() {
+            $elChk.change(function() {
+                var isAgree = this.checked;
+                if (isAgree) {
+                    $elSubmit.addClass("outerglow");
+                    $elSubmit.removeClass("btn-disable");
+                }
+                else {
+                    $elSubmit.removeClass("outerglow");
+                    $elSubmit.addClass("btn-disable");
+                }
+            });
+        };
+
+        var submit = function() {
+
+            $elSubmit.click(function() {
+                if ($elChk[0].checked) {
+                    validNull();
+                }
+                return false;
+            });
+        };
+
+        var $elMail = $("#fb-mail");
+        var $elName = $("#fb-username");
+        var $elPass = $("#fb-password");
+        var $elSubmit = $("#fb-createuser");
+        var $elChk = $("#fb-chk-protocal");
+
+        var valid = function() {
+            validEmail();
+            validUserName();
+            validPassword();
+            check();
+            submit();
+        };
+
+        this.init = function() {
+            valid();
+        };
+    };
+
+    ZM.UI.Form = {
+
+        init: function() {
+
+            var form = new Form;
+            form.init();
+
+        }
+
+    };
+
+})(jQuery);
