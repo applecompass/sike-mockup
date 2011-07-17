4fd633cd2ebafb5b954f33b7f231161f17eb31f5
Vince Liu <applecompass@gmail.com>
2011-07-17 18:31:47 +0800

    add autofocus (html5 support)

--- _scripts/page/login.js
+++ _scripts/page/login.js
@@ -7,17 +7,42 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Document Ready.
-jQuery(function($) {
+(function ($) {
 
+    var Form = function () {
+
+        var hint = function () {
+            ZM.Dom.textHint('#input-mail', '#input-mail-hint', true);
+            ZM.Dom.textHint('#input-password', '#input-password-hint', true);
+        };
+
+        var autoFocus = function () {
+            $("#input-mail").focus();
+        };
+
+        this.init = function () {
+            hint();
+            autoFocus();
+        };
+
+    };
+
+    ZM.UI.Login = {
+
+        init: function () {
+
+            var form = new Form;
+            form.init();
+
+        }
+
+    };
+
+})(jQuery);
+
+// Document Ready.
+jQuery(function ($) {
+
+    ZM.UI.Login.init();
 
 });
-
-// Window Onload.
-ZM.windowOnload = window.onload;
-window.onload = function() {
-    if (ZM.windowOnload) {
-        ZM.windowOnload();
-    }
-    // Your code here
-};
